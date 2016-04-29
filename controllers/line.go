62a56927f3bd60efc23fe1a45605bc61ba5d9922
upamune <jajkeqos@gmail.com>
2016-04-29 14:57:55 +0900

    [from now] 2016/04/29 14:57:55
    diff --git a/controllers/line.go b/controllers/line.go
    index 31f8556..b89e8c7 100644
    --- a/controllers/line.go
    +++ b/controllers/line.go
    @@ -36,6 +36,7 @@ func LineCallBackHandler(c echo.Context) error {
     				case linebot.ContentTypeText:
     					// すでにifでチェックしているので，ここのエラーは握り潰しても良い
     					textContent, _ := content.TextContent()
    +					log.Infof(ctx, "Receive:Text:%s:%s", content.From, textContent.Text)
     					res, err := bot.SendText([]string{content.From}, textContent.Text+"ぷり")
     					if err != nil {
     						log.Errorf(ctx, "Send Message Text Error: %v", err)
    @@ -44,6 +45,7 @@ func LineCallBackHandler(c echo.Context) error {
     					log.Infof(ctx, "Succeed Sent Messages: %v", res)
     				case linebot.ContentTypeImage:
     					res, err := bot.SendText([]string{content.From}, "いい写真ぷり!")
    +					log.Infof(ctx, "Receive:Image:%s:%s", content.From)
     					if err != nil {
     						log.Errorf(ctx, "Send Message Text Error: %v", err)
     						return
    @@ -51,6 +53,8 @@ func LineCallBackHandler(c echo.Context) error {
     					log.Infof(ctx, "Succeed Sent Messages: %v", res)
     				case linebot.ContentTypeLocation:
     					res, err := bot.SendText([]string{content.From}, "素敵な場所ぷりね")
    +					locationContent, _ := content.LocationContent()
    +					log.Infof(ctx, "Receive:Location:%s:%s", content.From, locationContent.Address)
     					if err != nil {
     						log.Errorf(ctx, "Send Message Text Error: %v", err)
     						return
    @@ -58,6 +62,8 @@ func LineCallBackHandler(c echo.Context) error {
     					log.Infof(ctx, "Succeed Sent Messages: %v", res)
     				case linebot.ContentTypeSticker:
     					res, err := bot.SendText([]string{content.From}, "いいスタンプぷり! みれぃのスタンプも買って欲しいぷり💕")
    +					stickerContent, _ := content.StickerContent()
    +					log.Infof(ctx, "Receive:Sticker:%s:%d:%d:%d", content.From, stickerContent.PackageID, stickerContent.ID, stickerContent.Version)
     					if err != nil {
     						log.Errorf(ctx, "Send Message Text Error: %v", err)
     						return

--- controllers/line.go
+++ controllers/line.go
@@ -36,6 +36,7 @@ func LineCallBackHandler(c echo.Context) error {
 				case linebot.ContentTypeText:
 					// すでにifでチェックしているので，ここのエラーは握り潰しても良い
 					textContent, _ := content.TextContent()
+					log.Infof(ctx, "Receive:Text:%s:%s", content.From, textContent.Text)
 					res, err := bot.SendText([]string{content.From}, textContent.Text+"ぷり")
 					if err != nil {
 						log.Errorf(ctx, "Send Message Text Error: %v", err)
@@ -44,6 +45,7 @@ func LineCallBackHandler(c echo.Context) error {
 					log.Infof(ctx, "Succeed Sent Messages: %v", res)
 				case linebot.ContentTypeImage:
 					res, err := bot.SendText([]string{content.From}, "いい写真ぷり!")
+					log.Infof(ctx, "Receive:Image:%s:%s", content.From)
 					if err != nil {
 						log.Errorf(ctx, "Send Message Text Error: %v", err)
 						return
@@ -51,6 +53,8 @@ func LineCallBackHandler(c echo.Context) error {
 					log.Infof(ctx, "Succeed Sent Messages: %v", res)
 				case linebot.ContentTypeLocation:
 					res, err := bot.SendText([]string{content.From}, "素敵な場所ぷりね")
+					locationContent, _ := content.LocationContent()
+					log.Infof(ctx, "Receive:Location:%s:%s", content.From, locationContent.Address)
 					if err != nil {
 						log.Errorf(ctx, "Send Message Text Error: %v", err)
 						return
@@ -58,6 +62,8 @@ func LineCallBackHandler(c echo.Context) error {
 					log.Infof(ctx, "Succeed Sent Messages: %v", res)
 				case linebot.ContentTypeSticker:
 					res, err := bot.SendText([]string{content.From}, "いいスタンプぷり! みれぃのスタンプも買って欲しいぷり💕")
+					stickerContent, _ := content.StickerContent()
+					log.Infof(ctx, "Receive:Sticker:%s:%d:%d:%d", content.From, stickerContent.PackageID, stickerContent.ID, stickerContent.Version)
 					if err != nil {
 						log.Errorf(ctx, "Send Message Text Error: %v", err)
 						return
